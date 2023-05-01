a40e92f94e5da9819305a1e2e7adf6d2df7586ef
rs dahiya <rsd74191197182gmail.com>
2023-05-01 17:41:23 +0530

    assoc array program using rec uploaded

--- arrayr_recursion_task.php
+++ arrayr_recursion_task.php
@@ -31,60 +31,46 @@ $arr=array(
         )
     )
 );
-foreach($arr as $k => $v){
-    $s=gettype($v);
-    if($s=='array'){
-        foreach($v as $j => $p){
-            $q=gettype($p);
-            if($q=='array'){
-                foreach($p as $a =>$b){
-                    $g=gettype($b);
+// foreach($arr as $k => $v){
+//     $s=gettype($v);
+//     if($s=='array'){
+//         foreach($v as $j => $p){
+//             $q=gettype($p);
+//             if($q=='array'){
+//                 foreach($p as $a =>$b){
+//                     $g=gettype($b);
   
-                    if($g=='array'){
-                        foreach($b as $e => $c){
-                            $t=gettype($c);
-                            if($t=='array'){}
-                            else{
-                                echo $c;
-                                echo ",";
-                            }
-                        }
+//                     if($g=='array'){
+//                         foreach($b as $e => $c){
+//                             $t=gettype($c);
+//                             if($t=='array'){}
+//                             else{
+//                                 echo $c;
+//                                 echo ",";
+//                             }
+//                         }
                         
-                    }
-                    else{
-                        echo $b;
-                        echo ",";
-                    }
-                }
+//                     }
+//                     else{
+//                         echo $b;
+//                         echo ",";
+//                     }
+//                 }
 
                 
-            }
-            else{
-                echo $p;
-                echo ",";
-            }
-        }
-    }
-    else{
-        echo $v;
-        echo ",";
-    }
-
-}
-// function rec($arr){
-//     foreach($arr as $k => $v){
-//         $s=gettype($v);
-//         if($s=='array'){
-//         rec($v);
+//             }
+//             else{
+//                 echo $p;
+//                 echo ",";
+//             }
 //         }
-//         else{
-//             echo $v;
-//             echo ",";
-//         }
+//     }
+//     else{
+//         echo $v;
+//         echo ",";
 //     }
 
 // }
-// rec($arr);
 
 
 ?>
